ddd36c43162aca78a2172c8ace1e9fbdfaf775ae
JesseBot <jessebot@linux.com>
2023-08-09 09:25:52 +0200

    Update config.js -  fix renovatebot to not be in dry run mode

--- .github/config.js
+++ .github/config.js
@@ -4,7 +4,7 @@ module.exports = {
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   platform: 'github',
   includeForks: true,
-  dryRun: 'full',
+  dryRun: null,
   repositories: ['jessebot/bitwarden-eso-provider'],
     extends: ['config:base'],
     allowPostUpgradeCommandTemplating: true,
